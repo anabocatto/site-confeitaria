6ed9f8314b554824b29f6d4a31ef56048bbf8d9a
Bia <anabeatrizbocatto@gmail.com>
2024-09-24 08:37:08 -0300

    site atualizado

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ def cardapio():
 def sobre():
     return render_template("sobre.html")
 
+@app.route("/contato")
+def contato():
+    return render_template("contato.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
